c8fb547f5dc4814057e56d204dcafae0c97c7c30
Ilya <ilya.blan4@gmail.com>
2014-01-18 14:21:47 +0700

    first non-repeated char and fix last commit

---
+++ ruby/easy/armstrong_numbers.rb
@@ -0,0 +1,34 @@
+<<-DOC
+ARMSTRONG NUMBERS
+CHALLENGE DESCRIPTION:
+An Armstrong number is an n-digit number that is equal to the sum of the n'th powers of its digits. Determine if the input numbers are Armstrong numbers.
+
+INPUT SAMPLE: Your program should accept as its first argument a path to a filename. Each line in this file has a positive integer. E.g.
+6
+153
+351
+
+OUTPUT SAMPLE: Print out True/False if the number is an Armstrong number or not. E.g.
+True
+True
+False
+DOC
+
+class Integer
+  def split_by_digits(base)
+    number = self
+    digits = []
+    begin
+      digits.push(number % base)
+      number /= base
+    end until number.zero?
+
+    digits
+  end
+end
+
+File.readlines(ARGV.first).each do |line|
+  number = line.to_i
+  arm = number.split_by_digits(10).map{|digit| digit**number.to_s.length}.reduce(:+)
+  puts number.eql?(arm) ? 'True' : 'False'
+end
---
+++ ruby/moderate/fitst_nonrepeated_char.rb
@@ -0,0 +1,27 @@
+<<-DOC
+FIRST NON-REPEATED CHARACTER
+CHALLENGE DESCRIPTION:
+Write a program to find the first non repeated character in a string.
+
+INPUT SAMPLE: The first argument will be a path to a filename containing strings. E.g.
+yellow
+tooth
+
+OUTPUT SAMPLE: Print to stdout, the first non repeating character, one per line. E.g.
+y
+h
+DOC
+File.readlines(ARGV.first).each do |line|
+  line
+    .split('')
+    .group_by { |letter| letter }
+    .map {|letter, repeats| {letter: letter, repeats: repeats.length} }
+    .each do |info|
+      if info[:repeats] == 1
+        puts info[:letter]
+        break
+      else
+        next
+      end
+    end
+end
